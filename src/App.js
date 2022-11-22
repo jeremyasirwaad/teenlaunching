ded203d07f0521f216dc0960d0d923839a1c427f
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2022-11-23 02:36:39 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import bg from "./bg.png";
+import logotin from "./Group 859.png";
+
+function App() {
+	return (
+		<div className="App">
+			<span>Launch Tinarosario.com</span>
+			<a href="https://tina-rosario-vt7x.vercel.app/">
+				<div className="btncont">
+					<img src={logotin} alt="" />
+				</div>
+			</a>
+		</div>
+	);
+}
+
+export default App;
